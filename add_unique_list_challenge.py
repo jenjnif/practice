08e09dc44c12fa60d9d1a971e54b3d829a400568
jenjnif <jenjnif@hotmail.com>
2020-10-27 06:55:02 +0000

    loop and print list

--- add_unique_list_challenge.py
+++ add_unique_list_challenge.py
@@ -1,3 +1,10 @@
 # add up the two unique values in array
 
 input_array = [4,5,7,5,4,8]
+dictionary = {}
+
+def add_unique_numbers():
+    for number in input_array:
+        print(number)
+
+add_unique_numbers()
