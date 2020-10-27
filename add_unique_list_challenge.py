32299e1d07c0605ceddb4decfabf59238306a444
jenjnif <jenjnif@hotmail.com>
2020-10-27 07:23:32 +0000

    solve with dictionary

--- add_unique_list_challenge.py
+++ add_unique_list_challenge.py
@@ -4,8 +4,12 @@ input_array = [4,5,7,5,4,8]
 dictionary = {}
 
 def add_unique_numbers():
+    unique = 0
     for number in input_array:
         dictionary[number] = dictionary.get(number, 0) + 1
-    return dictionary
+    for (key, value) in dictionary.items():
+        if value == 1:
+            unique += key
+    return unique
 
 print(add_unique_numbers())
