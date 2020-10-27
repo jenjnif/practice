9a026d9932115a0929e8bd49cb2ebd39789d5356
jenjnif <jenjnif@hotmail.com>
2020-10-27 13:33:50 +0000

    create set function to return set of array

--- add_unique_list_challenge.py
+++ add_unique_list_challenge.py
@@ -3,7 +3,7 @@
 input_array = [4,5,7,5,4,8]
 dictionary = {}
 
-def add_unique_numbers():
+def add_unique_numbers_dict(input_array):
     unique = 0
     for number in input_array:
         dictionary[number] = dictionary.get(number, 0) + 1
@@ -12,4 +12,15 @@ def add_unique_numbers():
             unique += key
     return unique
 
-print(add_unique_numbers())
+print(add_unique_numbers_dict(input_array))
+
+
+have_seen = set()
+have_not_seen = set()
+
+def add_unique_numbers_set(input_array):
+    for number in input_array:
+        have_seen.add(number)
+    return have_seen
+
+print(add_unique_numbers_set(input_array))
