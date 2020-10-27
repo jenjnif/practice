d09a42f173b871c590f66e2819b408de3bf0e6c1
jenjnif <jenjnif@hotmail.com>
2020-10-27 13:44:05 +0000

    solve with set

--- add_unique_list_challenge.py
+++ add_unique_list_challenge.py
@@ -16,11 +16,13 @@ print(add_unique_numbers_dict(input_array))
 
 
 have_seen = set()
-have_not_seen = set()
 
 def add_unique_numbers_set(input_array):
     for number in input_array:
-        have_seen.add(number)
-    return have_seen
+        if number in have_seen:
+            have_seen.remove(number)
+        else:
+            have_seen.add(number)
+    return sum(have_seen)
 
 print(add_unique_numbers_set(input_array))
